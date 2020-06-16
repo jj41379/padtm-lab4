9d2ac6db43263f302b060b71977e9d1fef5e295f
jj41379 <jj41379@zut.edu.pl>
2020-06-16 23:29:19 +0200

    Dodane obsługę formularza dodawania

--- app/src/main/java/com/example/padtm_lab4/DodajWpis.java
+++ app/src/main/java/com/example/padtm_lab4/DodajWpis.java
@@ -2,13 +2,24 @@ package com.example.padtm_lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class DodajWpis extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dodaj_wpis2);
+        setContentView(R.layout.activity_dodaj_wpis);
+    }
+
+    public void wyslij (View view) {
+        EditText kontrolka = (EditText)findViewById(R.id.editText);
+        String pole = kontrolka.getText().toString();
+        Intent intencja = new Intent();
+        intencja.putExtra("wpis", pole);
+        setResult(RESULT_OK, intencja);finish();
     }
 }
