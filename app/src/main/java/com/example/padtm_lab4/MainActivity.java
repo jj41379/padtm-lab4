bf170486ffed939c967a212507160a56e8597cab
jj41379 <jj41379@zut.edu.pl>
2020-06-16 23:18:41 +0200

    dodano activity "DodajWpis"

--- app/src/main/java/com/example/padtm_lab4/MainActivity.java
+++ app/src/main/java/com/example/padtm_lab4/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.padtm_lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -22,5 +26,17 @@ public class MainActivity extends AppCompatActivity {
         this.target.addAll(Arrays.asList(values));
         this.adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, this.target);
         ListView listview = (ListView) findViewById( R.id.listView );listview.setAdapter(this.adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public void nowyWpis(MenuItem mi) {
+        Intent intencja = new Intent(this, DodajWpis.class);
+        startActivityForResult(intencja, 1);
     }
 }
